060ea0dc3af6e77f21ba5205f76ba1d59622f58e
Socrates Osorio Diaz <socratesj.osorio@gmail.com>
2024-09-15 04:00:39 -0400

    Add convex init

--- web/brain_visualizer/src/components/Model.tsx
+++ web/brain_visualizer/src/components/Model.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Canvas, useLoader } from "@react-three/fiber";
 import { Environment, OrbitControls } from "@react-three/drei";
 import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { PLYLoader } from "three/examples/jsm/loaders/PLYLoader";
 
 export default function Model(props: any) {
     const [path, setPath] = useState('');
@@ -11,28 +11,37 @@ export default function Model(props: any) {
     }, [props.path]);
 
 function ModelB(props: React.ComponentPropsWithoutRef<"object3D">) {
-  const gltf = useLoader(
-    GLTFLoader,
-    "./models/pointcloud3.glb",
-    (loader) => {
-      // Register plugin to load all materials as red basic material
-      loader.register((parser) => ({
-        name: "customPlugin",
-        async loadMaterial() {
-          return new THREE.MeshBasicMaterial({ color: "red" });
-        },
-      }));
-    }
-  );
+    const path = "./models/heatmap_point_cloud.ply";
+    const material = new THREE.MeshStandardMaterial({
+        vertexColors: true,  // Enable vertex colors
+        side: THREE.DoubleSide,  // Optional: make sure both sides of the mesh are rendered
+      });
+  
+    const geometry = useLoader(PLYLoader, path);
 
-  return <primitive {...props} object={gltf.scene} /* rotation={[Math.PI / 2, 0, Math.PI / 2] } */ /* scale={100}  */ position={[10, -70, 50]} /* rotation={[0, Math.PI / 2, 0]} */ />;
+    
+    // (loader) => {
+    //   // Register plugin to load all materials as red basic material
+    //   loader.register((parser) => ({
+    //     name: "customPlugin",
+    //     async loadMaterial() {
+    //       return new THREE.MeshBasicMaterial({ color: "red" });
+    //     },
+    //   }));
+    // }
+    return (
+        <mesh material={material}>
+          <primitive object={geometry} attach="geometry" />
+        </mesh>
+      );
+
+//   return <primitive {...props} object={gltf.scene} /* rotation={[Math.PI / 2, 0, Math.PI / 2] } */ /* scale={100}  */ position={[10, -70, 50]} /* rotation={[0, Math.PI / 2, 0]} */ />;
 }
 
     return (
         <div>
-            <Canvas style={{ position: "absolute", inset: 0}}>
+            <Canvas style={{ position: "absolute", inset: 0, background: "white" }}>
                 <ModelB position-x={1} />
-                <Environment preset='sunset' background={"only"} />
                 <OrbitControls />
             </Canvas>
             <button
